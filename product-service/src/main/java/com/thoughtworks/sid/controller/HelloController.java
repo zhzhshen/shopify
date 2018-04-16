713a9a8ed9e62095d34cd01cb3078ee98b47ec99
Sid <sidchina.ire@gmail.com>
2018-04-16 16:02:59 +0800

    enable sso on product service, which leads to failure of api-gateway

--- product-service/src/main/java/com/thoughtworks/sid/controller/HelloController.java
+++ product-service/src/main/java/com/thoughtworks/sid/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.sid.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloController {
     @RequestMapping("/")
     public String hello(Principal user) {
         return "hello " + user.getName();
+    }
+
+    @RequestMapping("/hello")
+    public String user(HttpServletRequest request) {
+        return "hello2 " + request.getUserPrincipal().getName();
     }
 }
